866728c246001b728e16b61fcb084b88e9a370fb
Prafful Kumar <kumarprafful.54@gmail.com>
2017-11-12 00:01:03 +0530

    added descrition

--- store/models.py
+++ store/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class Item(models.Model):
-	name = models.CharField(max_length=100)
-	cost = models.IntegerField()
-	image = models.ImageField(upload_to='items')
+	item_type = models.CharField(max_length=255)
+	item_name = models.CharField(max_length=255)
+	item_cost = models.IntegerField()
+	item_description = models.TextField(max_length=5024)
+	item_image = models.ImageField(upload_to='items')
+	date_added = models.DateField(default=datetime.now)
 
 	def __str__(self):
-		return self.name
+		return self.item_type + ' - ' + self.item_name
