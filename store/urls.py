89c24b6ea6cdc46963cd51d03db9b03f1483e017
Prafful Kumar <kumarprafful.54@gmail.com>
2017-11-13 23:13:37 +0530

    cart carted

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import ItemListView, ItemDetailView
+from .views import ItemListView, ItemDetailView, product_detail
 
 app_name = 'store'
 
 urlpatterns = [
 	url(r'^$', ItemListView.as_view(), name='index'),
 	url(r'^(?P<pk>[0-9]+)/$', ItemDetailView.as_view(), name='item'),
+	# url(r'^prodetail/(?P<id>[0-9]+)/$', product_detail, name='product_detail')
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404
 from .models import Item
 
 from cart.forms import CartAddProductForm
