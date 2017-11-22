701d463a92b274ee0a3264448ce29cf4c1c3316e
Prafful Kumar <kumarprafful.54@gmail.com>
2017-11-22 16:27:26 +0530

    2.0.0

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# class Team(models.Model):
+# 	name = models.CharField(max_length=255)
+# 	image = models.ImageField(upload_to='team')
+# 	designation = models.CharField(max_length=255)
+# 	team_type = models.CharField(max_length=255)
+# 	facebook = models.URLField(null=True)
+# 	github = models.URLField(null=True)
+# 	linkedin =  models.URLField(null=True)
+
+# 	def __str__(self):
+# 		return self.name + self.designation
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+# from .models import Team
 # Create your views here.
 def index(request):
+	# team = Team.objects.all()
 	return render(request, 'home/index.html')
